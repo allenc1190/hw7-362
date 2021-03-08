8ebde2877e8146ae3b369d6342cf4e28ee284ad5
Allen Chan <chanall@oregonstate.edu>
2021-03-07 22:32:39 -0800

    updated code, pass test

--- leapyear.py
+++ leapyear.py
@@ -10,3 +10,28 @@ def leapYear(year):
 
     else:
         return False
+
+def checkLeapYear():
+    while True:
+        try:
+            year = int(input("Enter year between 0-9999: "))
+            if 0 <= year <= 9999:
+                break
+            raise ValueError()
+
+        except ValueError:
+            print("Enter number between 0 and 9999")
+
+    if year %4 == 0 and year % 100 != 0:
+        print(year, "is a leap year")
+    
+    elif year % 400 == 0:
+        print(year, "is a leap year")
+    
+    elif year % 100 == 0:
+        print(year, "is not a leap year")
+    
+    else:
+        print(year, "is not a leap year")
+
+checkLeapYear()
